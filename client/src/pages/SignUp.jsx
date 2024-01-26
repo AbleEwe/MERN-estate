dea32f1e3c3781be7fe7ccee3c2cd7c4abaa3dab
AbleEwe <125688963+AbleEwe@users.noreply.github.com>
2024-01-25 22:07:46 -0600

    completed sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,17 +1,25 @@
+import useSignUp from "../hooks/useSignUp"
 import { Link } from "react-router-dom"
 
 const SignUp = () => {
+  const {
+    error,
+    loading,
+    handleChance,
+    handleSubmit,
+  } = useSignUp();
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input type="text" placeholder="username" 
-        className="border p-3 rounded-lg" id='username'/>
+        className="border p-3 rounded-lg" id='username' onChange={handleChance}/>
         <input type="text" placeholder="email" 
-        className="border p-3 rounded-lg" id='email'/>
+        className="border p-3 rounded-lg" id='email' onChange={handleChance}/>
         <input type="text" placeholder="password" 
-        className="border p-3 rounded-lg" id='password'/>
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-opacity-95 disabled:opacity-80">Sign up</button>
+        className="border p-3 rounded-lg" id='password' onChange={handleChance}/>
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:bg-opacity-95 disabled:opacity-80">{loading ? 'Loading...' : 'Sign up'}</button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
@@ -19,6 +27,7 @@ const SignUp = () => {
         <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   )
 }
