0dceae4c8f99cfc4ca8479731c0ef332f61f83d1
AbleEwe <125688963+AbleEwe@users.noreply.github.com>
2024-01-31 20:03:08 -0600

    created user update api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.route.js'
 import cors from 'cors';
+import cookieParser from 'cookie-parser'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_DB)
@@ -18,6 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
