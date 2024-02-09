7270cba0e660b02e20eedd599887e5f6fde433d5
AbleEwe <125688963+AbleEwe@users.noreply.github.com>
2024-02-09 01:29:49 -0600

    Added DELETE method and functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,7 +21,6 @@ const Profile = () => {
   const [updateSuccess, setUpdateSuccess] = useState(false)
   const [showListingsError, setShowListingsError] = useState(false);
   const [userListings, setUserListings] = useState([]);
-  console.log(userListings)
   const {
     setFile,
     filePerc,
@@ -99,6 +98,22 @@ const Profile = () => {
       setShowListingsError(true);
     }
   }
+  
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+        const data = await res.json();
+        if(data.success === false) {
+          console.log(error.message)
+          return;
+    }
+    setUserListings((prev) => prev.filter((listing)=> listing._id !== listingId))
+    } catch (error) {
+      console.log(error.message);
+    }
+}
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -145,7 +160,7 @@ const Profile = () => {
               <p>{listing.name}</p>
             </Link> 
             <div className='flex flex-col items-center'>
-              <button className='text-red-700 uppercase'>Delete</button>
+              <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
               <button className='text-green-700 uppercase'>Edit</button>
             </div> 
           </div>
